1223d26819411e5cfb0e1e9dc01dcfa7b73e3f4c
Андрей Марков <e.markova8484@gmail.com>
2025-11-17 16:32:45 +0300

    Эпизод 2: Возвращение класса Core и создание диагарммы БД

---
+++ pr7/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr7
+{
+    internal class Core
+    {
+        public static pr7_Entities Context = new pr7_Entities();
+    }
+}
